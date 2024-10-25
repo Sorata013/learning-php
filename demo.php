cbbad8e3f1076fc2ca5c704484bfc642236f060c
Alejandro <sorata.arctica@gmail.com>
2024-10-25 12:21:27 +0200

    uso de una API

---
+++ demo.php
@@ -0,0 +1,72 @@
+<?php
+#Creamos una constante con la URL de la API que vamos a utilizar:
+const API_URL = "https://whenisthenextmcufilm.com/api";
+#Inicializamos una nueva sesión de cURL; ch = CURL handle:
+$ch = curl_init(API_URL);
+//Indicaremos a php que queremos recibir el resultado de la petición pero no mostrarlo en pantalla:
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//Ejecutamos la petición y guardamos el resultado en una variable:
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+curl_close($ch);
+
+/*
+ * Si sólo queremos hacer un GET de una API exite la siguiente manera para ahorrar código:
+ * $result = file_get_contents(API_URL);
+ */
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>la próxima película de marvel</title>
+    <meta name="description" content="La próxima película de Marvel"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/>
+    <style>
+        body{
+            display: grid;
+            place-content: center;
+        }
+        img{
+            margin: 0 auto;
+        }
+        section{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header style="text-align: center">
+        <h1>¿CUÁNDO LLEGA LA PRÓXIMA PELÍCULA DE MARVEL?</h1>
+    </header>
+    <main>
+        <section>
+            <!--<pre><?php var_dump($data);?></pre>-->
+            <h2>Quedan <?=$data["days_until"] ?> días para:</h2>
+            <img src="<?=$data["poster_url"]; ?>"
+                 width="300" alt="Poster de <?=$data["title"]; ?>"
+                 style="border-radius: 16px"
+            /><br>
+            <h3><?=$data["title"];?> </h3>
+            <h3>Fecha de estreno: <?=$data["release_date"]?></h3>
+            <h4><?=$data["overview"];?></h4>
+            <h5>La siguiente es: <?= $data["following_production"]["title"];?></h5>
+        </section>
+
+    </main>
+
+
+
+
+
+</body>
+</html>
+
